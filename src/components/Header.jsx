a4f3f9e8355f1e4c31c226944682234cb0a7e64b
EvgeniyWoolovich <ruswool2033@gmail.com>
2023-03-09 23:02:18 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://d2j6dbq0eux0bg.cloudfront.net/images/16279212/990891707.jpg' />
+        </header>
+    )
+}
+
+export default Header;
